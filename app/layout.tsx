1a264f49946bab4aab9ac2542b0755921e63ab23
RobelHailu <robel.hailu@seattlecolleges.edu>
2023-07-03 17:40:06 -0700

    root layout editing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/Navbar';
+import SessionProvider from '@/components/SessionProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>
+          <Navbar/>{children}
+        </SessionProvider>
+      </body>
     </html>
   )
-}
+};
