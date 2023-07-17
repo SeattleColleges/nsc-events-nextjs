bcd79154ca97199c6c6222a75eb72462aee0c3bf
Gary Larson <gary.larson@seattlecolleges.edu>
2023-07-17 15:38:37 -0700

    Ci cd 7 added unit test (#26)
    * Create dependabot.yml
    
    * Added compile script to run typescript compiler
    
    * main file that runs both ci and cd workflows on merged PR to staging
    
    * CI: clean install, ts compile check, linting
    
    * removed caret to stay with fixed versioning standard
    
    * functional but empty CD workflow
    
    * config jest to work with ts
    
    * dummy method and test to validate jest unit tests are working
    
    * added scripts to run jest unit testing in workflows
    
    * added unit testing to ci workflow
    
    * added subtract test
    
    ---------
    
    Co-authored-by: theGaryLarson <gary.larson316@gmail.com>
    Co-authored-by: Nicklas Di Pietro <37163331+Seiyaroo@users.noreply.github.com>

--- tests/mocks/math.ts
+++ tests/mocks/math.ts
@@ -1,4 +1,8 @@
 export function add(a: number, b: number): number {
     console.log('test')
     return a + b;
+
+export function subtract(a: number, b: number): number {
+    console.log('test')
+    return a - b;
 }
