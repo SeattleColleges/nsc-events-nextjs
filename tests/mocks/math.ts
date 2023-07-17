0509aeea73b3e6a694567793bd93ffc0dff6e417
theGaryLarson <gary.larson316@gmail.com>
2023-07-17 15:29:04 -0700

    added subtract test

--- tests/mocks/math.ts
+++ tests/mocks/math.ts
@@ -2,3 +2,8 @@ export function add(a: number, b: number): number {
     console.log('test')
     return a + b;
 }
+
+export function subtract(a: number, b: number): number {
+    console.log('test')
+    return a - b;
+}
