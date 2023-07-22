f3d197960777e713b6dce2074301e12441ecbeaf
theGaryLarson <gary.larson316@gmail.com>
2023-07-21 19:27:55 -0700

    fix: curly brace was deleted and accidentally pushed.

--- tests/mocks/math.ts
+++ tests/mocks/math.ts
@@ -1,6 +1,7 @@
 export function add(a: number, b: number): number {
     console.log('test')
     return a + b;
+}
 
 export function subtract(a: number, b: number): number {
     console.log('test')
