4deeebdcf2961eb80b97e2b8f608f00ba5430bb9
Gary Larson <gary.larson@seattlecolleges.edu>
2023-08-07 14:34:53 -0700

    feat: created json object to represent an event. (#43)

---
+++ models/event.js
@@ -0,0 +1,32 @@
+const event = {
+    eventCreatorId: "",
+    eventTitle: "",
+    eventDescription: "",
+    eventCategory: "",
+    eventDate: "",
+    eventStartTime: "",
+    eventEndTime: "",
+    eventLocation: "",
+    eventCoverPhoto: "",
+    eventHost: "",
+    eventWebsite: "",
+    eventRegistration: "",
+    eventCapacity: 0,
+    eventCost: "",
+    eventTags: [],
+    eventSchedule: "",
+    eventSpeakers: [],
+    eventPrerequisites: "",
+    eventCancellationPolicy: "",
+    eventContact: "",
+    eventSocialMedia: {
+        facebook: "",
+        twitter: "",
+        instagram: "",
+        hashtag: ""
+    },
+    eventPrivacy: "",
+    eventAccessibility: ""
+}
+
+export default event;
