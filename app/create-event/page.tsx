61e713d62a38315e1c2b809d8439d004da47f156
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-08 18:59:14 -0700

    event creation page

---
+++ app/create-event/page.tsx
@@ -0,0 +1,106 @@
+"use client";
+import {ChangeEvent, ChangeEventHandler, FormEvent, useState} from "react";
+import TagSelector from "@/components/TagSelector"
+import {Event} from "@/models/activity";
+import activityAutofill from "@/models/activityAutofill";
+
+const CreateEvent = () => {
+    const [eventData, setEventData] = useState<Event>(activityAutofill);
+
+    const handleInputChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+        const { name, value } = target;
+        setEventData(prevEventData => ({
+            ...prevEventData,
+            [name]: value
+        }));
+    };
+
+    const handleTagClick = (tag: string) => {
+        setEventData(prevEventData => {
+            if (prevEventData.eventTags.includes(tag)) {
+                // If the tag is already selected, remove it from the array
+                return {
+                    ...prevEventData,
+                    eventTags: prevEventData.eventTags.filter(t => t !== tag)
+                };
+            } else {
+                // If the tag is not selected, add it to the array
+                return {
+                    ...prevEventData,
+                    eventTags: [...prevEventData.eventTags, tag]
+                };
+            }
+        });
+    };
+
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // Save the event in the db through nest.js
+        console.log(eventData);
+    };
+
+    return (
+        <div className="ml-4">
+            <form onSubmit={handleSubmit} className="space-y-4 mr-4">
+                <div>
+                    <h2 className="text-lg font-bold mt-4">General Information</h2>
+                    <div className="space-y-2">
+                        <label>
+                            Event Title
+                            <input name="eventTitle" value={eventData.eventTitle} onChange={handleInputChange}
+                                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                        </label>
+                        <label>
+                            Event Category
+                            <input name="eventCategory" value={eventData.eventCategory} onChange={handleInputChange}
+                                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                        </label>
+                        <div className="space-y-2">
+                            <TagSelector selectedTags={eventData.eventTags}
+                                         allTags={['Professional Development', 'Social', 'Tech', 'Conference', 'Networking', 'Pizza', 'LGBTQIA']}
+                                         onTagClick={handleTagClick}/>
+
+                        </div>
+                        {/* Repeat for other fields in the "General Information" group */}
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="text-lg font-bold">Event Schedule</h2>
+                    <div className="space-y-2">
+                        {/* Add fields for eventDate, eventStartTime, eventEndTime, eventSchedule */}
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="text-lg font-bold">Event Details</h2>
+                    <div className="space-y-2">
+                        {/* Add fields for eventLocation, eventHost, eventWebsite, eventRegistration, eventCapacity, eventCost, eventSpeakers, eventPrerequisites, eventCancellationPolicy */}
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="text-lg font-bold">Contact & Accessibility</h2>
+                    <div className="space-y-2">
+                        {/* Add fields for eventContact, eventPrivacy, eventAccessibility */}
+                    </div>
+                </div>
+
+                <div>
+                    <h2 className="text-lg font-bold">Event Media</h2>
+                    <div className="space-y-2">
+                        {/* Add fields for eventCoverPhoto, eventSocialMedia */}
+                    </div>
+                </div>
+
+                <button type="submit" className="px-4 py-2 text-white bg-indigo-600 rounded hover:bg-indigo-700">Create
+                    Event
+                </button>
+            </form>
+        </div>
+    );
+};
+
+
+export default CreateEvent;
