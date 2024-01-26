8cb3072e45d558c2df592fed39df3d5449569ca7
Ali Sharif <alisharif29@gmail.com>
2024-01-25 23:01:46 -0800

    Fixed lint issues

--- components/ReactQueryProvider.tsx
+++ components/ReactQueryProvider.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 
 
-const ReactQueryProvider = ({children}: {children: React.ReactNode}) => {
+const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
     const queryClient = new QueryClient
 
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
+        <QueryClientProvider client={ queryClient }>
+            { children }
         </QueryClientProvider>
     )
     
