707e0d1536f7f1fb0fc01f208ec42cddf0e0d343
RobelHailu <robel.hailu@seattlecolleges.edu>
2024-01-25 19:33:38 -0800

    input fields have been added, but not all

--- app/create-event/page.tsx
+++ app/create-event/page.tsx
@@ -7,6 +7,7 @@ import activityAutofill from "@/models/activityAutofill";
 //test
 const CreateEvent = () => {
     const [eventData, setEventData] = useState<Activity>(activityAutofill);
+    const [errors, setErrors] = useState({});
 
     const handleInputChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
         const { name, value } = target;
@@ -15,6 +16,7 @@ const CreateEvent = () => {
             [name]: value
         }));
     };
+
 
     const handleTagClick = (tag: string) => {
         setEventData(prevEventData => {
@@ -38,9 +40,29 @@ const CreateEvent = () => {
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(eventData);
-        createActivity(eventData);
-
+        const newErrors = validateFormData(eventData);
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+        } else {
+            createActivity(eventData);
+        }
     };
+
+    const validateFormData = (data: Activity) => {
+        let newErrors = {};
+        // basic validation rules, extend as needed
+        if (!data.eventTitle) {
+            newErrors = { ...newErrors, eventTitle: 'Event title is required' };
+        }
+        if (!data.eventDescription) {
+            newErrors = { ...newErrors, eventDescription: 'Event description is required' };
+        }
+
+        // todo: add more error validation rules
+
+        return newErrors;
+
+    }
 
     const createActivity = async (activityData: any) => {
         try {
@@ -70,23 +92,47 @@ const CreateEvent = () => {
                 <div>
                     <h2 className="text-lg font-bold mt-4">General Information</h2>
                     <div className="space-y-2">
-                        <label>
-                            Event Title
-                            <input name="eventTitle" value={eventData.eventTitle} onChange={handleInputChange}
-                                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
-                        </label>
-                        <label>
-                            Event Category
-                            <input name="eventCategory" value={eventData.eventCategory} onChange={handleInputChange}
-                                   className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
-                        </label>
+                            <label>
+                                Event Title
+                                <input 
+                                    type="text"
+                                    name="eventTitle" 
+                                    value={eventData.eventTitle} 
+                                    onChange={handleInputChange}
+                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                    />    
+                                {/* todo: add the errors checking for eventDescription */}
+                            </label>
+                            <label>
+                                Event Description
+                                <input 
+                                    type="text"
+                                    name="eventDescription"
+                                    value={eventData.eventDescription}
+                                    onChange={handleInputChange}
+                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                />
+                                {/* todo: add the errors checking for eventCategory */}
+                            </label>
+                            <label>
+                                Event Category
+                                <input 
+                                    type="text"
+                                    name="eventCategory" 
+                                    value={eventData.eventCategory} 
+                                    onChange={handleInputChange}
+                                    className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                                />
+                                {/* todo: add the errors checking for eventCategory */}
+                            </label>
                         <div className="space-y-2">
-                            <TagSelector selectedTags={eventData.eventTags}
-                                         allTags={['Professional Development', 'Social', 'Tech', 'Conference', 'Networking', 'Pizza', 'LGBTQIA']}
-                                         onTagClick={handleTagClick}/>
-
+                            <TagSelector 
+                                selectedTags={eventData.eventTags}
+                                allTags={['Professional Development', 'Social', 'Tech', 'Conference', 'Networking', 'Pizza', 'LGBTQIA']}
+                                onTagClick={handleTagClick}/>
                         </div>
                         {/* Repeat for other fields in the "General Information" group */}
+
                     </div>
                 </div>
 
@@ -94,6 +140,48 @@ const CreateEvent = () => {
                     <h2 className="text-lg font-bold">Event Schedule</h2>
                     <div className="space-y-2">
                         {/* Add fields for eventDate, eventStartTime, eventEndTime, eventSchedule */}
+                        <label>
+                            Event Date
+                            <input 
+                                type="date"
+                                name="eventDate" 
+                                value={eventData.eventDate} 
+                                onChange={handleInputChange}
+                                className="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"/>
+                        </label>
+
+                        <label>
+                            Event Start Time
+                            <input 
+                                type="time"
+                                name="eventStartTime"
+                                value={eventData.eventStartTime}
+                                onChange={handleInputChange}
+                                className="block w-full mt-1" 
+                            />
+                        </label>
+
+                        <label>
+                            Event End Time
+                            <input 
+                                type="time"
+                                name="eventEndTime"
+                                value={eventData.eventEndTime}
+                                onChange={handleInputChange}
+                                className="block w-full mt-1" 
+                            />
+                        </label>
+
+                        <label>
+                            Event Schedule
+                            <input 
+                                type="text"
+                                name="eventSchedule"
+                                value={eventData.eventSchedule}
+                                onChange={handleInputChange}
+                                className="block w-full mt-1" 
+                            />
+                        </label>
                     </div>
                 </div>
 
@@ -101,6 +189,18 @@ const CreateEvent = () => {
                     <h2 className="text-lg font-bold">Event Details</h2>
                     <div className="space-y-2">
                         {/* Add fields for eventLocation, eventHost, eventWebsite, eventRegistration, eventCapacity, eventCost, eventSpeakers, eventPrerequisites, eventCancellationPolicy */}
+
+                        <label>
+                            Event Location
+                            <input 
+                                type="text"
+                                name="eventLocation"
+                                value={eventData.eventLocation}
+                                onChange={handleInputChange}
+                                className="block w-full mt-1" 
+                            />
+                        </label>
+                        
                     </div>
                 </div>
 
@@ -115,6 +215,7 @@ const CreateEvent = () => {
                     <h2 className="text-lg font-bold">Event Media</h2>
                     <div className="space-y-2">
                         {/* Add fields for eventCoverPhoto, eventSocialMedia */}
+                        
                     </div>
                 </div>
 
