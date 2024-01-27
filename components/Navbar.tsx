93a64e4ccd42e7e7df59af100e6a2a6ea78db254
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2024-01-27 04:04:30 -0800

    Added Items to Navbar and implemented DrawerComp with Material UI for responsive navigation (#135)
    Co-authored-by: BC Ko <bcko@users.noreply.github.com>
    Co-authored-by: E. Brink <80282265+brinkbrink@users.noreply.github.com>

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,65 @@
-import Profile from "@/app/profile/page";
-import Link from "next/link";
-import React from "react";
-import AuthProfileMenu from "./AuthProfileMenu";
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useSession, signOut } from "next-auth/react";
 import Image from 'next/image';
 import logo from '../app/logo.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, IconButton, Grid, Button } from '@mui/material';
+import DrawerComp from './DrawerComp'; 
 
 export default function Navbar() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const { status } = useSession();
+  const isAuth = status === "authenticated";
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && (event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift') {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
   return (
-    <nav className="flex items-center max-w-screen-lg mx-auto px-5 py-2 shadow-md justify-between mt-2 mb-2 rounded">
-      <a href="/"><Image src={logo} alt="logo" width={40} height={40}/></a>
-      <Link href="/">Home</Link>
-      <AuthProfileMenu />      
-    </nav>
-  )
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <a href="/"><Image src={logo} alt="logo" width={40} height={40} /></a>
+          </Grid>
+          <Grid item>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+              sx={{ display: { xs: 'block', md: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Grid container spacing={2} sx={{ display: { xs: 'none', md: 'flex' } }}>
+              <Grid item>
+                <Link href="/" passHref>
+                  <Button color="inherit" style={{ textTransform: 'none' }}>Home</Button>
+                </Link>
+              </Grid>
+              {isAuth && (
+                <>
+                  <Grid item>
+                    <Link href="/create-event" passHref>
+                      <Button color="inherit" style={{ textTransform: 'none' }}>Create Event</Button>
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Button color="inherit" style={{ textTransform: 'none' }} onClick={() => signOut({ callbackUrl: "/" })}>Sign out</Button>
+                  </Grid>
+                </>
+              )}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Toolbar>
+      <DrawerComp isOpen={drawerOpen} toggleDrawer={toggleDrawer} isAuth={isAuth} />
+    </AppBar>
+  );
 };
