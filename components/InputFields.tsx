cc537d33ad9ef76d48a344e9c0238e17d4a770d7
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2024-01-30 04:38:39 -0800

    Refactored InputFields component to use Material-UI TextField (#153)
    Co-authored-by: hanan o <77591083+heosman@users.noreply.github.com>

--- components/InputFields.tsx
+++ components/InputFields.tsx
@@ -1,6 +1,8 @@
 import React, { ChangeEventHandler } from "react";
-// TODO: Clean this up
-// TODO see which CSS framework are we using
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+
 interface InputFieldProps {
   label: string;
   type: string;
@@ -25,33 +27,34 @@ const InputField: React.FC<InputFieldProps> = ({
   togglePasswordVisibility,
 }) => {
   return (
-    <div className="mb-4 relative">
-      <label htmlFor={name} className="block mb-1 font-semibold">
-        {label}
-      </label>
-      <div className="flex items-center">
-        <input
-          id={name}
-          className={`w-full px-4 py-2 border ${
-            error ? "border-red-500" : "border-gray-300"
-          } rounded-md focus:outline-none focus:border-blue-500 text-black pr-10`}
-          type={type}
-          name={name}
-          value={value}
-          onChange={onChange}
-          required={required}
-        />
-        {icon && (
-          <div className="absolute inset-y-0 right-1 pr-3 flex items-center text-sm leading-5">
-            <span onClick={togglePasswordVisibility} className="cursor-pointer">
+    <TextField
+      label={label}
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      required={required}
+      error={!!error}
+      helperText={error}
+      fullWidth
+      variant="outlined"
+      margin="normal"
+      InputProps={{
+        endAdornment: togglePasswordVisibility && icon ? (
+          <InputAdornment position="end">
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={togglePasswordVisibility}
+              edge="end"
+            >
               {icon}
-            </span>
-          </div>
-        )}
-      </div>
-      {error && <p className="text-red-400 text-sm mt-1">{error}</p>}
-    </div>
+            </IconButton>
+          </InputAdornment>
+        ) : null,
+      }}
+    />
   );
 };
+
 
 export default InputField;
