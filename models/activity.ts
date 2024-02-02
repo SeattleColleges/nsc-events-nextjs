ad8ae28ab68aec607d1713533dbd759880eae5f5
Ali Sharif <alisharif29@gmail.com>
2024-02-02 00:02:20 -0800

    Remove "activity.ts"

--- models/activity.ts
+++ models/activity.ts
@@ -1,33 +1,32 @@
 export interface Activity {
-    eventCreatorId: string,
-    eventTitle: string,
-    eventDescription: string,
-    eventCategory: string,
-    eventDate: string,
-    eventStartTime: string,
-    eventEndTime: string,
-    eventLocation: string,
-    eventCoverPhoto: string,
-    eventHost: string,
-    eventWebsite: string,
-    eventRegistration: string,
-    eventCapacity: number,
-    eventCost: string,
-    eventTags: string[],
-    eventSchedule: string,
-    eventSpeakers: string[],
-    eventPrerequisites: string,
-    eventCancellationPolicy: string,
-    eventContact: string,
+    eventCreatorId: string;
+    eventTitle: string;
+    eventDescription: string;
+    eventCategory: string;
+    eventDate: string;
+    eventStartTime: string;
+    eventEndTime: string;
+    eventLocation: string;
+    eventCoverPhoto: string;
+    eventHost: string;
+    eventWebsite: string;
+    eventRegistration: string;
+    eventCapacity: number;
+    eventCost: string;
+    eventTags: string[];
+    eventSchedule: string;
+    eventSpeakers: string[];
+    eventPrerequisites: string;
+    eventCancellationPolicy: string;
+    eventContact: string;
     eventSocialMedia: {
         facebook: string;
         twitter: string;
         instagram: string;
         hashtag: string;
-    },
-    eventPrivacy: string,
-    eventAccessibility: string,
-    event: any
+    };
+    eventPrivacy: string;
+    eventAccessibility: string;
 }
 
 export const activity: Activity = {
