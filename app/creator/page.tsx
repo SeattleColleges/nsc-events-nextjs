8076cbb8a420275e985897dcf0a648e019600f97
Intisar Osman <intisar.Osman@seattlecolleges.edu>
2024-02-04 22:58:52 -0800

    nsc-events-nextjs_2_167_creator-page-buttons
    Added buttons for creator page

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
+import styles from "./creator-page.module.css";
 
 const Creator = () => {
   // Temporary boilerplate code to make it compile
   return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-          <h1>Placeholder for the creator page so npm run build compiles successfully.</h1>
-           <p>FIX: move to pages or use getSession from nextauth</p>
-           <p>FIX: allow only users with creator role to be routed to this page</p>
+    <div className={styles.container}>
+        {/* <h1>Placeholder for the creator page so npm run build compiles successfully.</h1>
+          <p>FIX: move to pages or use getSession from nextauth</p>
+          <p>FIX: allow only users with creator role to be routed to this page</p> */}
+          <button className={styles.button}>Edit User Role</button>
+          <button className={styles.button}><Link href="/create-event">Create Event</Link></button>
+          <button className={styles.button}>View Events</button>
       </div>
   );
 };
