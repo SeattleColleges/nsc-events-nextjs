c3f3ba5ebb6da730e2b3c2390663bfb570d8ad46
Jeremy Ward <exochos@gmail.com>
2024-02-12 11:25:48 -0800

    Update validateSignUp.tsx
    Added error checking for all of the current fields.

--- app/auth/sign-up/validateSignUp.tsx
+++ app/auth/sign-up/validateSignUp.tsx
@@ -1,6 +1,60 @@
-interface SignUpForm {  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  confirmPassword: string;
+interface SignUpForm {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    }
+
+export const validateSignUp = (values: SignUpForm) => {
+
+    const errors: Partial<SignUpForm> = {};
+
+    const { firstName, lastName, email, password, confirmPassword } = values;
+
+    if (!firstName) {
+        errors.firstName = "First name is required";
+    }
+    if (firstName.length > 30) {
+        errors.firstName = "First name must be less than 30 characters";
+    }
+    
+    if (!lastName) {
+        errors.lastName = "Last name is required";
+    }
+    if (lastName.length > 30) {
+        errors.lastName = "Last name must be less than 30 characters";
+    }
+
+    if (!email) {
+        errors.email = "Email is required";
+    }
+
+    if (!password) {
+        errors.password = "Password is required";
+    }
+
+    if (!confirmPassword) {
+        errors.confirmPassword = "Confirm password is required";
+    } else if (password.length < 8) {
+        errors.password = "Password must be at least 8 characters";
+    } else if (password.length > 30) {
+        errors.password = "Password must be less than 30 characters";
+    } else if (password.search(/[a-z]/i) < 0) {
+        errors.password = "Password must contain at least one letter";
+    } else if (password.search(/[0-9]/) < 0) {
+        errors.password = "Password must contain at least one digit";
+    } else if (password.search(/[!@#$%^&*]/) < 0) {
+        errors.password = "Password must contain at least one special character";
+    }
+
+    if (confirmPassword !== password) {
+        errors.confirmPassword = "Passwords do not match";
+    }
+
+    if (email && !/^\S+@\S+\.\S+$/.test(email)) {
+        errors.email = "Invalid email address";
+    }
+
+    return errors;
 }
