53435d02a59dfa87023aca19505551dd166e1af5
Jeremy Ward <exochos@gmail.com>
2024-02-12 11:18:01 -0800

    Added interface to utility

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -72,35 +72,7 @@ const SignUp = () => {
 
     // Validate inputs before form submission
     // TODO add more validation
-    if (!firstName) {
-      newErrors.firstName = "First name is required";
-    }
-    if (!lastName) {
-      newErrors.lastName = "Last name is required";
-    }
-    if (!email) {
-      newErrors.email = "Email is required";
-    }
-    if (!password) {
-      newErrors.password = "Password is required";
-    }
-    if (!confirmPassword) {
-      newErrors.confirmPassword = "Confirm password is required";
-    } else if (password.length < 10) {
-      newErrors.password = "Password must be at least 8 characters";
-    } else if (password.length > 20) {
-      newErrors.password = "Password must be less than 20 characters";
-    } else if (password.search(/[a-z]/i) < 0) {
-      newErrors.password = "Password must contain at least one letter";
-    } else if (password.search(/[0-9]/) < 0) {
-      newErrors.password = "Password must contain at least one digit";
-    } else if (password.search(/[!@#$%^&*]/) < 0) {
-      newErrors.password =
-        "Password must contain at least one special character";
-    }
-    if (confirmPassword !== password) {
-      newErrors.confirmPassword = "Passwords do not match";
-    }
+
 
     // Update the state with the new errors
     setErrors(newErrors);
---
+++ app/auth/sign-up/validateSignUp.tsx
@@ -0,0 +1,6 @@
+interface SignUpForm {  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
