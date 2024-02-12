5fb902cded88c236aca6d99b2227b1360c5e7f91
NahomAlemu <55855783+NahomAlemu@users.noreply.github.com>
2024-02-12 10:49:50 -0800

    nsc-events-nextjs_2_162_refactor-sign-in-page-and-enhance-UI-consistency (#175)
    * refactored sign-in page and enhance UI consistency
    
    * updated the use of an apostrophe within a JSX string
    
    * removed CssBaseline component to keep style simple
    
    * removed CssBaseline component to keep style simple
    
    * Update package.json to with new packages

---
+++ app/theme.ts
@@ -0,0 +1,19 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+import { blue, green } from '@mui/material/colors';
+
+const theme = createTheme({
+  // Customize your theme here
+  palette: {
+    primary: {
+      main: blue[800], //alternatively '#1565c0'
+    },
+    secondary: {
+      main: green[500], 
+    },
+    // Add additional theme configurations as needed
+  },
+  // You can also customize other theme aspects like typography, breakpoints, etc.
+});
+
+export default theme;
