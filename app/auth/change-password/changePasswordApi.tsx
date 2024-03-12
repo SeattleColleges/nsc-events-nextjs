db92dbd328587e77ad94c96242887f96c85cedc3
Tuong Pham Minh <77989401+TroyFPV48@users.noreply.github.com>
2024-03-12 01:22:12 -0700

    password change page created, make sure your back-end is up to date which has change-password endpoints

---
+++ app/auth/change-password/changePasswordApi.tsx
@@ -0,0 +1,58 @@
+const URL = "http://localhost:3000/api";
+
+interface ChangePasswordModel {
+    currentPassword: string;
+    newPassword: string;
+    newPasswordConfirm: string;
+}
+
+interface ChangePasswordSuccessResponse {
+  status: "success";
+  message: string;
+  token?: string;
+}
+
+interface ChangePasswordErrorResponse {
+  status: "error";
+  message: string;
+}
+
+type ChangePasswordResponse = ChangePasswordSuccessResponse | ChangePasswordErrorResponse;
+
+export const changePassword = async (
+  model: ChangePasswordModel, // password form will be passed into here 
+  userToken: string // user token
+): Promise<ChangePasswordResponse> => {
+  try {
+    const res = await fetch(`${URL}/auth/change-password`, {
+      method: "POST",
+      headers: 
+      { 
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${userToken}` //passing bearer token from local storage into headers
+      },
+      body: JSON.stringify(model),
+    });
+
+    const data = await res.json(); //get response, expecting a successful message
+
+    if (res.ok) { //successful password change
+      return {
+        status: "success",
+        message: "Password Changed successful!",
+      };
+    } else {
+      // Other errors with a specific message from the backend
+      return {
+        status: "error",
+        message: data.error || "An error occurred during password change.",
+      };
+    }
+  } catch (error) {
+    console.error("Error changing password:", error);
+    return {
+      status: "error",
+      message: "Failed to connect to the change password function.",
+    };
+  }
+};
---
+++ app/auth/change-password/page.tsx
@@ -0,0 +1,249 @@
+"use client";
+
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
+import Image from "next/image";
+import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
+import { Container, Paper, Box, TextField, Button, Typography, InputAdornment, IconButton, Link as MuiLink } from "@mui/material";
+import { textFieldStyle } from "@/components/InputFields";
+import { validateChangePassword } from "./validatePassword";
+import { changePassword } from "./changePasswordApi";
+import React from "react";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import NorthSeattleLogo from "../../NorthSeattleLogo.png";
+
+interface State extends SnackbarOrigin {
+    open: boolean;
+  }
+
+const ChangePassword = () => {
+    
+    // Set initial state for password visibility
+    const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+    const [showNewPassword, setShowNewPassword] = useState(false);
+    const [showNewPasswordConfirm, setNewPasswordConfirm] = useState(false);
+
+    // Toggle password visibility
+    const toggleCurrentPasswordVisibility = () => {
+        setShowCurrentPassword(!showCurrentPassword);
+    };
+    const toggleNewPasswordVisibility = () => {
+        setShowNewPassword(!showNewPassword);
+    };
+    const togglePasswordConfirmVisibility = () => {
+        setNewPasswordConfirm(!showNewPasswordConfirm);
+    };
+
+    const [pageInfo, setPageInfo] = useState({
+        currentPassword: "",
+        newPassword: "",
+        newPasswordConfirm: "",
+  
+      });
+
+    // Set initial state for errors
+    const [errors, setErrors] = useState<Partial<typeof pageInfo>>({
+        currentPassword: "",
+        newPassword: "",
+        newPasswordConfirm: "",
+    });
+
+    // Set initial state for snackbar message
+    const [snackBarMessage, setSnackBarMessage] = useState("");
+    
+  
+    const { currentPassword, newPassword, newPasswordConfirm } = pageInfo;
+  
+    const handleChange: ChangeEventHandler<HTMLInputElement> = ({ target }) => {
+        const { name, value } = target;
+        setErrors({ ...errors, [name]: "" });
+        setPageInfo({ ...pageInfo, [name]: value }); // set page info from the form
+    };
+  
+    const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+
+        // Validate user input
+        let errors = validateChangePassword(pageInfo);
+        setErrors(errors);
+        const hasErrors = Object.values(errors).some((error) => error !== "");
+        // If there are errors, do not submit the form
+        if (hasErrors) {
+            return;
+        }
+            
+        // Generate form for password change
+        const passwordChangeForm = {
+            currentPassword,
+            newPassword,
+            newPasswordConfirm,
+        };
+
+        const userToken = localStorage.getItem("token");
+        if (userToken) {
+            changePassword(passwordChangeForm, userToken)
+              .then(response => {
+                console.log(response.message);
+                // Handle success
+                if (response.status === "success" && response.token) {         
+                    setSnackBarMessage(response.message);
+                } else {
+                    setSnackBarMessage(response.message);
+                }
+              })
+              .catch(error => {
+                // Handle error
+                console.error(error);
+              });
+          } else {
+            console.error('No user token available');
+          }
+    };
+  
+    return (
+      <Container maxWidth="xs"
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            height: "100vh",
+            justifyContent: "center",
+        }}
+      >
+        <Paper
+            elevation={6}
+            sx={{ padding: 4, width: "100%", borderRadius: 2, mb: 15 }}
+        >
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                }}
+            >
+                <Image
+                    src={NorthSeattleLogo.src}
+                    alt="North Seattle College Logo"
+                    width={150}
+                    height={50}
+                    style={{ borderRadius: "10px" }}
+                />
+                <Typography component="h2" variant="h6">
+                    Change Password
+                </Typography>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="currentPassword"
+                    label="Current Password"
+                    type={showCurrentPassword ? "text" : "password"}
+                    id="currentPassword"
+                    autoComplete="current-password"
+                    value={pageInfo.currentPassword}
+                    onChange={handleChange}
+                    error={Boolean(errors.currentPassword)}
+                    helperText={errors.currentPassword}
+                    InputLabelProps={{ style: textFieldStyle.label }}
+                    InputProps={{
+                        style: textFieldStyle.input, 
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password"
+                            onClick={toggleCurrentPasswordVisibility}
+                            >
+                            {showCurrentPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="newPassword"
+                    label="New Password"
+                    type={showNewPassword ? "text" : "password"}
+                    id="newPassword"
+                    autoComplete="new-password"
+                    value={pageInfo.newPassword}
+                    onChange={handleChange}
+                    error={Boolean(errors.newPassword)}
+                    helperText={errors.newPassword}
+                    InputLabelProps={{ style: textFieldStyle.label }}
+                    InputProps={{
+                        style: textFieldStyle.input, 
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password"
+                            onClick={toggleNewPasswordVisibility}
+                            >
+                            {showNewPassword ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="newPasswordConfirm"
+                    label="New Password Confirm"
+                    type={showNewPasswordConfirm ? "text" : "password"}
+                    id="newPasswordConfirm"
+                    autoComplete="new-password"
+                    value={pageInfo.newPasswordConfirm}
+                    onChange={handleChange}
+                    error={Boolean(errors.newPasswordConfirm)}
+                    helperText={errors.newPasswordConfirm}
+                    InputLabelProps={{ style: textFieldStyle.label }}
+                    InputProps={{
+                        style: textFieldStyle.input, 
+                        endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password"
+                            onClick={togglePasswordConfirmVisibility}
+                            >
+                            {showNewPasswordConfirm ? <VisibilityOff /> : <Visibility />}
+                            </IconButton>
+                        </InputAdornment>
+                        ),
+                    }}
+                />
+                <Button
+                    style={{ textTransform: 'none' }}
+                    color="primary"
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Change Password
+                </Button>
+                <Box textAlign="center">
+                    <MuiLink href="/auth/forgot-password" variant="body2">
+                        Forgot password?
+                    </MuiLink>
+                </Box>
+            </Box>
+        </Paper>
+        <Snackbar
+            open={Boolean(snackBarMessage)}
+            autoHideDuration={6000}
+            onClose={() => setSnackBarMessage("")}
+            anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            message={snackBarMessage}
+        />
+      </Container>
+    );
+  };
+  
+  export default ChangePassword;
