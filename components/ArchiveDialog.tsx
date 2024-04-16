8e254ebbc461890780c794a6273e9283cc8406f7
Ali Sharif <alisharif29@gmail.com>
2024-04-16 12:47:09 -0700

    Added ArchiveDialog component and archive event functionality

---
+++ components/ArchiveDialog.tsx
@@ -0,0 +1,89 @@
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import { Button } from "@mui/material";
+import React, { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import Snackbar from "@mui/material/Snackbar";
+
+interface ArchiveDialogProps {
+    isOpen: boolean;
+    eventId: String;
+    dialogToggle: () => void;
+}
+
+const ArchiveDialog = ({ isOpen, eventId, dialogToggle }: ArchiveDialogProps) => {
+    const router = useRouter();
+    const  [snackbarMessage, setSnackbarMessage] = useState("")
+
+    const archiveEvent = async () => {
+        const token = localStorage.getItem("token");
+        try {
+            const response = await fetch(`http://localhost:3000/api/events/archive/${eventId}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            return response.json();
+        } catch (error) {
+            console.error('error: ', error)
+        }
+    }
+
+    const { mutate: archiveEventMutation }  = useMutation({
+        mutationFn: archiveEvent,
+        onSuccess: () => {
+            setSnackbarMessage("Successfully archived event.");
+            setTimeout( () => {
+                router.push("/");
+            }, 1200)
+
+        },
+        onError: (error: String) => {
+            setSnackbarMessage("Failed to archive event.");
+            console.error("Failed to archive event: ", error)
+        }
+    })
+
+    return (
+        <>
+            <Dialog
+                open={isOpen}>
+                <DialogTitle>
+                    {"Archive Event?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Are you sure you want to archive this event?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={() => {
+                        dialogToggle()
+                    }}>Cancel</Button>
+                    <Button onClick={() => {
+                        archiveEventMutation(eventId)
+                        dialogToggle()
+                    }} autoFocus>
+                        Archive
+                    </Button>
+                </DialogActions>
+            </Dialog>
+            <Snackbar
+                open={Boolean(snackbarMessage)}
+                onClose={() => {
+                    setSnackbarMessage("")
+                }}
+                autoHideDuration={1200}
+                anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+                message={snackbarMessage}
+            />
+        </>
+    )
+}
+export default ArchiveDialog;
