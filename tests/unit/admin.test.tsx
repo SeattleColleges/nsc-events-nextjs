2e9f0be8a86347ac7ec9212e4919da8b963beea9
Bejarano03 <marco.bejarano03@gmail.com>
2024-04-23 01:28:40 -0700

    created a test for admin page loading all buttons and checking for button with text Edit User Role

--- tests/unit/admin.test.tsx
+++ tests/unit/admin.test.tsx
@@ -5,7 +5,8 @@ import Page from '../../app/admin/page';
 describe('Admin', () => {
     it('renders the buttons', () => {
         render(<Page />)
-        const button = screen.getByRole('button')
-        expect(button).toBeInTheDocument()
+        const button = screen.getAllByRole('button')
+        const buttonText = screen.getByText('Edit User Role')
+        expect(button && buttonText).toBeInTheDocument()
     })
 })
