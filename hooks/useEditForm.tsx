f638ac46af84dcf229bc17f42b626dd40b049050
Ali Sharif <alisharif29@gmail.com>
2024-04-28 22:34:14 -0700

    Created useEditForm and EditDialog

---
+++ hooks/useEditForm.tsx
@@ -0,0 +1,153 @@
+import { FormEvent, useEffect, useState } from "react";
+import { validateFormData } from "@/utility/validateFormData";
+import useDateTimeSelection from "./useDateTimeSelection";
+import { ActivityDatabase } from "@/models/activityDatabase";
+import { useMutation } from "@tanstack/react-query";
+import { useEventForm } from "@/hooks/useEventForm";
+import { format, parse } from "date-fns";
+import { useRouter } from "next/navigation";
+
+export const useEditForm = (initialData: ActivityDatabase) => {
+  const router = useRouter();
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const {
+    setEventData,
+    setErrors,
+    errors,
+    errorMessage,
+    successMessage,
+    setErrorMessage,
+    setSuccessMessage,
+    eventData,
+    to12HourTime,
+    handleInputChange,
+    handleSocialMediaChange,
+    handleTagClick,
+  } = useEventForm(initialData);
+
+  useEffect(() => {
+    setEventData(initialData)
+    setSelectedDate(new Date(eventData.eventDate))
+  }, [eventData.eventDate, initialData, setEventData]);
+
+  const {
+    timeError,
+    handleStartTimeChange,
+    handleEndTimeChange,
+    startTime,
+    endTime
+  } = useDateTimeSelection(eventData.eventStartTime.slice(0, -2),  eventData.eventEndTime.slice(0, -2));
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newErrors = validateFormData(eventData);
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      editEventMutation(eventData);
+    }
+  };
+
+  const handleDateChange = (newDate: Date | null)  => {
+    setSelectedDate(newDate);
+  };
+
+const to24HourTime  = (time: string) => {
+    return parse(time, 'hh:mma', new Date());
+  }
+
+  // Handlers for TimePicker changes, converting Date back to string
+  const onStartTimeChange = (date: Date | null) => {
+    const timeStr = date ? format(date, 'HH:mm') : '';
+    handleStartTimeChange(timeStr);
+  };
+
+  const onEndTimeChange = (date: Date | null) => {
+    const timeStr = date ? format(date, 'HH:mm') : '';
+    handleEndTimeChange(timeStr);
+  };
+
+  const editEvent = async (activityData: ActivityDatabase) => {
+    // retrieving the token from localStorage
+    const token = localStorage.getItem('token');
+
+    // applying necessary transformations for date, time, speaker fields, and createdByUserId
+    const { createdByUser, ...dataToSend } = activityData;
+
+    if (selectedDate) {
+      dataToSend.eventDate = selectedDate.toISOString().split('T')[0];
+    }
+    if (startTime) {
+      dataToSend.eventStartTime = to12HourTime(startTime);
+    }
+    if (endTime) {
+      dataToSend.eventEndTime = to12HourTime(endTime);
+    }
+
+    if (typeof dataToSend.eventSpeakers === 'string') {
+      dataToSend.eventSpeakers = [dataToSend.eventSpeakers];
+    }
+
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/events/update/${dataToSend._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+        body: JSON.stringify(dataToSend),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log("Event updated:", data);
+        return response.status
+      } else {
+        throw new Error(data.message || "Failed to update event.");
+      }
+    } catch (error) {
+      console.error(error)
+      throw error;
+    }
+  }
+
+  const { mutate: editEventMutation }  = useMutation({
+    mutationFn: editEvent,
+    onSuccess: () => {
+      setSuccessMessage( "Event successfully updated!");
+      setTimeout( () => {
+        router.refresh();
+      }, 1200)
+
+    },
+
+    onError: (error: any) => {
+      if (error instanceof Error) {
+        console.error("Error updating event:", error);
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("An unexpected error occurred.");
+      }
+    }
+  })
+
+  return {
+    handleDateChange,
+    onStartTimeChange,
+    onEndTimeChange,
+    to24HourTime,
+    eventData,
+    handleInputChange,
+    handleSocialMediaChange,
+    handleTagClick,
+    handleSubmit,
+    errors,
+    selectedDate,
+    timeError,
+    successMessage,
+    errorMessage
+  };
+}
