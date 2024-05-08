b4909bb384cda5d32e13ec6edd12815ba0444d39
cshimm <cameronjshimmin@gmail.com>
2024-05-08 14:54:21 -0700

    fix formatDate logic

--- utility/dateUtils.ts
+++ utility/dateUtils.ts
@@ -2,6 +2,11 @@ export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
     // 8 hours ahead of Coordinated Universal Time (UTC)
     date.setUTCHours(8, 0, 0, 0);
-    const options: Intl.DateTimeFormatOptions = { timeZone: 'America/Los_Angeles', month: '2-digit', day: '2-digit', year: 'numeric' };
+    const options: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Los_Angeles',
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric'
+    };
     return date.toLocaleString('en-US', options);
 }
