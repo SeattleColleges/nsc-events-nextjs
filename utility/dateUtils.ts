d6cf162cd91a6aee077681f520233ddc00228f8e
Cameron Shimmin <cameronjshimmin@gmail.com>
2024-05-12 13:38:29 -0700

    nsc-events-nextjs-6-337-fix-incorrect-date-format-logic (#339)
    * fix formatDate logic
    
    * fix formatDate logic
    
    * update UTC hours to 16 and update comment

--- utility/dateUtils.ts
+++ utility/dateUtils.ts
@@ -1,7 +1,14 @@
 export const formatDate = (dateString: string) => {
     const date = new Date(dateString);
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    const year = date.getFullYear();
-    return `${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}-${year}`;
+    // PST is 8 hours behind Coordinated Universal Time (UTC).
+    // Therefore, we set hours to 16 since setUTCHours takes
+    // absolute hours values from 0-23.
+    date.setUTCHours(16, 0, 0, 0);
+    const options: Intl.DateTimeFormatOptions = {
+        timeZone: 'America/Los_Angeles',
+        month: '2-digit',
+        day: '2-digit',
+        year: 'numeric'
+    };
+    return date.toLocaleString('en-US', options);
 }
