11385f9ffded7074a56a8e60984bad07832b55d2
cshimm <cameronjshimmin@gmail.com>
2024-05-20 18:05:28 -0700

    display events on Archived Events page

--- app/archived-events/page.tsx
+++ app/archived-events/page.tsx
@@ -1,11 +1,35 @@
+'use client';
+
+import {useFilteredEvents} from "@/utility/queries";
+import {ActivityDatabase} from "@/models/activityDatabase";
+import EventCard from "@/components/EventCard";
+import {Container, Grid} from "@mui/material";
+import React from "react";
+import styles from '@/app/home.module.css'
 
 const ArchivedEvents = () => {
-  // Temporary boilerplate code to make it compile
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Archived Events</h1>
-    </div>
-  );
+    const {data} = useFilteredEvents()
+    return (
+        <Container maxWidth={false} className={styles.container}>
+            <p className={styles.title}>Archived Events</p>
+            <Grid
+                container
+                direction={'column'}
+                spacing={1}
+                alignItems={'center'}
+                justifyItems={'center'}
+            >
+                {
+                    data?.map((event: ActivityDatabase) => (
+                        <EventCard
+                            key={event._id}
+                            event={event}
+                        />
+                    ))
+                }
+            </Grid>
+        </Container>
+    );
 };
 
 export default ArchivedEvents;
