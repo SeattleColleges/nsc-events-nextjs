d5aef781d736eb137d5f312978a9dbfc91f4fafa
cshimm <cameronjshimmin@gmail.com>
2024-05-25 20:21:21 -0700

    update query logic to use tanstack query

--- components/HomeEventGetter.tsx
+++ components/HomeEventGetter.tsx
@@ -1,41 +1,25 @@
 'use client';
 
 import React, { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
 import { Grid, Button } from '@mui/material';
-import Link from "next/link";
 import { ActivityDatabase } from "@/models/activityDatabase";
 import EventCard from "./EventCard";
+import { useFilteredEvents } from "@/utility/queries";
 
 export function HomeEventsList(){
-    // const [numEvents, setNumEvents] = useState(5);
     const [page, setPage] = useState(1)
-    const [events, setEvents] = useState<ActivityDatabase[] | undefined>([]);
-    const [reachedMaxEvents, setReachedMaxEvents] = useState(false)
-    const getNumEvents = async (page:any) => {
-        const params = new URLSearchParams({
-            page: String(page),
-            isArchived: String(false),
-            isHidden: String(false)
-        });
-        const response = await fetch(`http://localhost:3000/api/events?${params.toString()}`);
-        return response.json();
-    };
+    const [events, setEvents] = useState<ActivityDatabase[]>([]);
+    const [reachedLastPage, setReachedLastPage] = useState(false);
+    const { data } = useFilteredEvents(page);
     useEffect(() => {
-        const eventData = getNumEvents(page);
-        eventData.then(events => {
-            setEvents(existing => existing?.concat(events) )
-            // setReachedMaxEvents(events.length < page*5);
-        });
-    }, [page]);
+        if (data) {
+            setEvents((prevEvents) => [...prevEvents, ...data]);
+            setReachedLastPage(data.length < 5);
+        }
+    }, [data]);
     const handleLoadMoreEvents = () => {
         setPage(num => num + 1);
     };
-    // if(isLoading) {
-    //     return <span>Loading events...</span>
-    // } else if (isError) {
-    //     return <span>Error when fetching events...</span>
-    // } else {
     return (
         <Grid container spacing={1}>
             {
@@ -47,7 +31,7 @@ export function HomeEventsList(){
 
                 ))}
             {
-                !reachedMaxEvents &&
+                !reachedLastPage &&
                 <Button onClick={handleLoadMoreEvents}
                         type='button'
                         variant="contained"
@@ -61,7 +45,6 @@ export function HomeEventsList(){
             }
         </Grid>
     );
-    // }
 }
 
 export default HomeEventsList;
--- utility/queries.ts
+++ utility/queries.ts
@@ -1,17 +1,17 @@
 import {useQuery} from "@tanstack/react-query";
 import {ActivityDatabase} from "@/models/activityDatabase";
 
-const getEvents = async() => {
-    const response = await fetch(`http://localhost:3000/api/events`);
+const getEvents = async(page: any) => {
+    const params = new URLSearchParams({
+        page: String(page),
+        isArchived: String(false),
+    });
+    const response = await fetch(`http://localhost:3000/api/events?${params.toString()}`);
     return response.json();
 }
-export function useFilteredEvents() {
+export function useFilteredEvents(page: any) {
     return useQuery<ActivityDatabase[], Error>({
-        queryKey: ["events"],
-        queryFn: getEvents,
-        select: (data: ActivityDatabase[]) => data.filter( event =>
-            !(event.isHidden) && !(event.isArchived))?.sort((event1, event2) => {
-            return new Date(event1.eventDate).getTime() - new Date(event2.eventDate).getTime();
-        }),
+        queryKey: ["events", page],
+        queryFn: () => getEvents(page),
     });
 }
