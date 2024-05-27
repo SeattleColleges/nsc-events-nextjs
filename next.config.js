075741731808c35a863336e181ea2dfaba492352
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-05-27 02:52:57 -0700

    Role-Authorize Create-event (#365)
    Co-authored-by: taylorpapke <94101953+taylorpapke@users.noreply.github.com>
    Co-authored-by: hanan o <77591083+heosman@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   env: {
     // TODO: PULL THIS FROM GITHUB ACTIONS
     // NSC_EVENTS_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-    NSC_EVENTS_PUBLIC_API_URL: "https://northseattlecollegeevents.com/api",
+    //NSC_EVENTS_PUBLIC_API_URL: "https://northseattlecollegeevents.com/api",
   },
 }
 
