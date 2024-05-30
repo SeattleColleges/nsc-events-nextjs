77b62f15db580687050e30e4b2a2660349e4b503
kaimanasse15 <91504156+kaimanasse15@users.noreply.github.com>
2024-05-30 09:26:02 -0700

    Nsc_events for My Event Page

--- app/my-events/page.tsx
+++ app/my-events/page.tsx
@@ -1,27 +1,31 @@
-"use client"
+"use client";
 
 import useAuth from "@/hooks/useAuth";
-import styles from "../home.module.css"
+import styles from "../home.module.css";
 import MyEventsList from "@/components/ViewMyEventsGetter";
+import UnauthorizedPages from "@/components/UnauthorizedPages";
 
 // Page to view logged-in user's created events (Admin/Creator ONLY)
 const MyEvents = () => {
-    const { isAuth, user } = useAuth()
-    // check if user is authorized to access page
-    if (isAuth && (user?.role == 'admin' || user?.role == 'creator')){
-        return(
-            <div className={styles.welcomeContainer}>
-                <div className={styles.title}>
-                    <h1>My Created Events</h1> 
-                </div>  
-                <div className={styles.eventContainer}>
-                    <div className={styles.homeEventsList}>
-                        <MyEventsList /> 
-                    </div>
-                </div>
-            </div>   
-        );
-    }
+  const { isAuth, user } = useAuth();
+
+  // check if user is authorized to access page
+  if (isAuth && (user?.role === 'admin' || user?.role === 'creator')) {
+    return (
+      <div className={styles.welcomeContainer}>
+        <div className={styles.title}>
+          <h1>My Created Events</h1> 
+        </div>  
+        <div className={styles.eventContainer}>
+          <div className={styles.homeEventsList}>
+            <MyEventsList /> 
+          </div>
+        </div>
+      </div>   
+    );
+  } else {
+    return <UnauthorizedPages />;
+  }
 };
 
-export default MyEvents
+export default MyEvents;
--- components/UnauthorizedPages.tsx
+++ components/UnauthorizedPages.tsx
@@ -3,9 +3,12 @@ import Typography from "@mui/material/Typography";
 
 const UnauthorizedPages: React.FC = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', flexDirection: 'column' }}>
       <Typography variant="h5" component="h2">
-      401 Unauthorized
+        401 Unauthorized
+      </Typography>
+      <Typography variant="h5" component="h2" sx={{ maxWidth: 500 }}>
+        You are not authorized to access this page
       </Typography>
     </Box>
   );
