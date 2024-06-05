f9ef9c42334cda35dc5cb88f502e8d0daf10ca85
Tin Pham <72054441+tinpham5614@users.noreply.github.com>
2024-06-05 07:23:23 -0700

    add dark/light mode (#424)
    Co-authored-by: hanan o <77591083+heosman@users.noreply.github.com>

---
+++ app/theme/themes.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
+let darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      light: "#3f50b5",
+      main: "#1565c0",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#6fbf73",
+      main: "#4caf50",
+      dark: "#357a38",
+      contrastText: "#fff",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: "#f5f5f5",
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#333",
+        },
+      },
+    }
+  },
+});
+darkTheme = responsiveFontSizes(darkTheme);
+
+let lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      light: "#3f50b5",
+      main: "#1565c0",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#6fbf73",
+      main: "#4caf50",
+      dark: "#357a38",
+      contrastText: "#fff",
+    },
+    background: {
+      default: "#EEE7DA",
+    },
+  },
+});
+lightTheme = responsiveFontSizes(lightTheme);
+
+export { darkTheme, lightTheme };
