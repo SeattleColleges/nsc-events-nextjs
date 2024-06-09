dbb14f44f990116a271914bc91b70958fa7eadd9
Cameron Shimmin <cameronjshimmin@gmail.com>
2024-06-08 20:40:01 -0700

    nsc-events-8-409-implement-material-ui (#439)
    * creator page mui conversion
    
    * admin mui conversion
    
    * added AdminButton
    
    * updated creator page
    
    * new box element admin to align top
    
    * new box element to align top creator
    
    * Center buttons on admin & creator page
    
    * Home page: loading and logged in page converted
    
    * Home page:logged out page converted
    
    * home page: margin bottom to title
    
    * archived events: converted title to MUI
    
    * my events: convert to MUI
    
    * change container to box
    
    * linter fixes
    
    * consistent styling on Archive Events page and My Events page
    
    * changed background to grey on admin and creator page
    
    * fix background widths for admin and creator page
    
    * fix home page title font size
    
    * importing global.css and home.module.css to admin page
    
    * importing global.css and home.module.css to creator page
    
    * Admin button fix
    
    * importing global.css and home.module.css to archived-events page
    
    * importing global.css to my-events page
    
    * fix my events page styles
    
    * fix archive events page styles
    
    * apply mui theme to home page, archived events, my events
    
    ---------
    
    Co-authored-by: Jesse <jesse.caddell@wsu.edu>
    Co-authored-by: hanan o <77591083+heosman@users.noreply.github.com>

--- app/my-events/page.tsx
+++ app/my-events/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import useAuth from "@/hooks/useAuth";
-import styles from "../home.module.css";
+import '../globals.css';
 import MyEventsList from "@/components/ViewMyEventsGetter";
+import { Box, Container, Typography } from "@mui/material";
 import UnauthorizedPageMessage from "@/components/UnauthorizedPageMessage";
+
 
 // Page to view logged-in user's created events (Admin/Creator ONLY)
 const MyEvents = () => {
@@ -11,14 +13,17 @@ const MyEvents = () => {
     // check if user is authorized to access page
     if (isAuth && (user?.role == 'admin' || user?.role == 'creator')) {
         return(
-            <div className={styles.welcomeContainer}>
-                <div className={styles.title}>
-                    <h1>My Created Events</h1> 
-                </div>  
-                <div className={styles.eventContainer}>
-                    <MyEventsList />
-                </div>
-            </div>   
+            <Container maxWidth={false} className="bg-solid">
+                <Typography
+                    variant={"h3"}
+                    textAlign={"center"}
+                    padding={"1rem"}
+                    marginBottom={"1rem"}
+                >
+                    My Created Events
+                </Typography>
+                <MyEventsList />
+            </Container>
         );
     } else {
       return <UnauthorizedPageMessage />;
