e8e655d61d929196b71c55f128cdf1ed264a80b3
Intisar Osman <intisar.Osman@seattlecolleges.edu>
2024-06-29 16:08:07 -0700

    Admin test

--- tests/unit/admin.test.tsx
+++ tests/unit/admin.test.tsx
@@ -1,12 +1,27 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from '../../app/admin/page';
+import useAuth from '@/hooks/useAuth'
 
+
+jest.mock('@/hooks/useAuth', () => ({
+    __esModule: true,
+    default: jest.fn(),
+  }))
+  
 describe('Admin', () => {
+    beforeEach(() => {
+        (useAuth as jest.Mock).mockReturnValue({
+          isAuth: true,
+          user: { role: 'admin' }
+        })
+      })
+
     it('renders the buttons', () => {
         render(<Page />)
         const button = screen.getAllByRole('button')
         const buttonText = screen.getByText('Edit User Role')
-        expect(button && buttonText).toBeInTheDocument()
+        expect(button.length).toBeGreaterThan(0)
+        expect(buttonText).toBeInTheDocument()
     })
 })
