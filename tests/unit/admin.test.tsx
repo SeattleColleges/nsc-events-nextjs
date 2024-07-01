9f54ecf8c8582a5262a58175aea237af50598590
Bejarano03 <marco.bejarano03@gmail.com>
2024-06-30 20:21:03 -0700

    importing necessary dependencies for the mock

--- tests/unit/admin.test.tsx
+++ tests/unit/admin.test.tsx
@@ -1,6 +1,8 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import Page from '../../app/admin/page';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 describe('Admin', () => {
     it('renders the buttons', () => {
