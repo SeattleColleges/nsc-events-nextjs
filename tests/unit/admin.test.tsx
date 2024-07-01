a8dcc865565b08bfa2c42e4bbc667a5ab628c97d
Bejarano03 <marco.bejarano03@gmail.com>
2024-06-30 20:21:31 -0700

    created the mocks

--- tests/unit/admin.test.tsx
+++ tests/unit/admin.test.tsx
@@ -4,6 +4,18 @@ import Page from '../../app/admin/page';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
+// Mock next/link component
+jest.mock('next/link', () => {
+    return ({ children, href }) => {
+        return <a href={href}>{children}</a>;
+    };
+});
+
+// Mock next/router
+jest.mock('next/router', () => ({
+    useRouter: jest.fn(),
+}));
+
 describe('Admin', () => {
     it('renders the buttons', () => {
         render(<Page />)
