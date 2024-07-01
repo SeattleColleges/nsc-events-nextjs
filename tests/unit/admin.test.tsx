ad47cbca4bcaf9c3b7b059e7347ec358b2127803
Bejarano03 <marco.bejarano03@gmail.com>
2024-06-30 20:22:30 -0700

    created first set of tests

--- tests/unit/admin.test.tsx
+++ tests/unit/admin.test.tsx
@@ -17,10 +17,23 @@ jest.mock('next/router', () => ({
 }));
 
 describe('Admin', () => {
+    const renderComponent = () => {
+        render(<Admin />);
+    };
+
     it('renders the buttons', () => {
         render(<Page />)
         const button = screen.getAllByRole('button')
         const buttonText = screen.getByText('Edit User Role')
         expect(button && buttonText).toBeInTheDocument()
     })
+
+    it('renders the Admin component', () => {
+        renderComponent();
+        expect(screen.getByText('Edit User Role')).toBeInTheDocument();
+        expect(screen.getByText('Create Event')).toBeInTheDocument();
+        expect(screen.getByText('View My Events')).toBeInTheDocument();
+        expect(screen.getByText('View Archived Events')).toBeInTheDocument();
+        expect(screen.getByText('View All Events')).toBeInTheDocument();
+    });
 })
