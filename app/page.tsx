bb720e2ef71b9df26c37b509547121740a89f830
heosman <hanan.osman@seattlecolleges.edu>
2024-07-10 21:41:31 -0700

    Simplify logic for rendering Welcome Container

--- app/page.tsx
+++ app/page.tsx
@@ -45,69 +45,52 @@ const Home = () => {
 
   return (
     <>
-      {token ? (
-        <Box>
-          <Typography
-              fontSize={"2.25rem"}
-              textAlign={"center"}
-              padding={"1rem"}
-              marginTop={"1rem"}
-              marginBottom={"1rem"}
-          >Upcoming Events
-          </Typography>
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Box sx={{ display: "flex", flex: "1", pl: "33vh" }}>
-              <HomeEventsList />
-            </Box>
-            <Box sx={{ display: "flex", flex: "1", pl: "4vh" }}>
-              <UpcomingEvent />
-            </Box>
-          </Box>
-        </Box>
-      ) : (
         <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", pt: "5vh" }}>
-          <Box sx={{ backgroundColor: containerColor, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", p: "2rem", borderRadius: "15px", width: "800px", mb: "10vh" }}>
-            <Box sx={{ display: "block", m: "0 auto", width: "100px", height: "100px", mb: "1rem" }}>
-            <Image src={imagePath} title={"NSC Logo"} alt={"NSC Logo"} width={100} height={100} />
-            </Box>
-            <Typography
-                fontSize={"2.25rem"}
-                textAlign={"center"}
-                padding={"1rem"}
-                marginBottom={"1.5rem"}
-            >Welcome to North Seattle College Events
-            </Typography>
-            <Box
-                flex={1}
-                display={"flex"}
-                gap={1}
-                marginBottom={"1em"}
-            >
-              <Link href="auth/sign-in">
-                <Button
-                    variant="contained"
-                    color="primary">Sign In
-                </Button>
-              </Link>
-              <Link href="auth/sign-up">
-                  <Button 
-                      variant="contained"
-                      color="primary">Sign Up
-                  </Button>
-              </Link>
-            </Box>
-            {/* download mobile app link */}
-            <Box style={{ marginBottom:"1em" }}>
-                <Link href="">
+          {!token ? (
+            <Box sx={{ backgroundColor: containerColor, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", p: "2rem", borderRadius: "15px", width: "800px", mb: "10vh" }}>
+              <Box sx={{ display: "block", m: "0 auto", width: "100px", height: "100px", mb: "1rem" }}>
+              <Image src={imagePath} title={"NSC Logo"} alt={"NSC Logo"} width={100} height={100} />
+              </Box>
+              <Typography
+                  fontSize={"2.25rem"}
+                  textAlign={"center"}
+                  padding={"1rem"}
+                  marginBottom={"1.5rem"}
+              >Welcome to North Seattle College Events
+              </Typography>
+              <Box
+                  flex={1}
+                  display={"flex"}
+                  gap={1}
+                  marginBottom={"1em"}
+              >
+                <Link href="auth/sign-in">
                   <Button
                       variant="contained"
-                      color="secondary">
-                    <Image src={google_play} alt="google_play" style={{ marginRight: "8px" }} />
-                    Download App
+                      color="primary">Sign In
                   </Button>
                 </Link>
-            </Box>
-          </Box>
+                <Link href="auth/sign-up">
+                    <Button 
+                        variant="contained"
+                        color="primary">Sign Up
+                    </Button>
+                </Link>
+              </Box>
+              {/* download mobile app link */}
+              <Box style={{ marginBottom:"1em" }}>
+                  <Link href="">
+                    <Button
+                        variant="contained"
+                        color="secondary">
+                      <Image src={google_play} alt="google_play" style={{ marginRight: "8px" }} />
+                      Download App
+                    </Button>
+                  </Link>
+              </Box>
+            </Box> 
+          ) : null }
+          <Box>
             <Typography
                 fontSize={"2.25rem"}
                 textAlign={"center"}
@@ -115,16 +98,16 @@ const Home = () => {
                 marginBottom={"1rem"}
             >Upcoming Events
             </Typography>
-          <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <Box sx={{ display: "flex", flex: "1", pl: "33vh" }}>
-              <HomeEventsList />
-            </Box>
-            <Box sx={{ display: "flex", flex: "1", pl: "4vh" }}>
-              <UpcomingEvent />
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+              <Box sx={{ display: "flex", flex: "1", ml: "30vh", mt: 1 }}>
+                <HomeEventsList />
+              </Box>
+              <Box sx={{ display: "flex", flex: "1", ml: 4 }}>
+                <UpcomingEvent />
+              </Box>
             </Box>
           </Box>
         </Box>
-      )}
     </>
   );
 };
