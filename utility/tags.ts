341e9e75c15bd1ff73463d8aca9f84d2541cd66c
Cameron Shimmin <cameronjshimmin@gmail.com>
2024-07-24 20:17:16 -0700

    nsc-events-nextjs-10-548-home-page-filter-events-by-tag (#553)
    * added tags.ts
    
    * updated tags list with EventTags from tags.ts
    
    * updated create-event TagSelector with EventTags
    
    * basic event filtering by event tag
    
    * tidy code
    
    * remove activeTags useEffect, moved activeTags to useFilteredEvents to use with query. update handleTagClicked, update events render logic
    
    * updated getEvents and useFilteredEvents functions to support tags
    
    * fix imports formatting
    
    ---------
    
    Co-authored-by: BC Ko <bcko@users.noreply.github.com>

---
+++ utility/tags.ts
@@ -0,0 +1,16 @@
+export const EventTags: string[] = [
+    "Professional Development",
+    "Club",
+    "Social",
+    "Tech",
+    "Cultural",
+    "Study",
+    "Coffee",
+    "Art/Creative",
+    "Conference",
+    "Craft",
+    "Networking",
+    "Pizza",
+    "Free Food",
+    "LGBTQIA",
+];
