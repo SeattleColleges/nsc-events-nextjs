bda358dea4de85f18c172b450b26fadf077a2609
BC Ko <bcko@users.noreply.github.com>
2024-07-28 18:59:12 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   env: { 
   // Double check which to comment out
     // TODO: PULL THIS FROM GITHUB ACTIONS
-    NSC_EVENTS_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,// Connects locally
-    //NSC_EVENTS_PUBLIC_API_URL: "https://northseattlecollegeevents.com/api",
+    // NSC_EVENTS_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,// Connects locally
+    NSC_EVENTS_PUBLIC_API_URL: "https://northseattlecollegeevents.com/api",
   },
 }
 
