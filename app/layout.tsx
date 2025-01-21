de08c0f55bbf035ca352267807ca3e250c7d882d
CourtneyHoppus <cobaltab@gmail.com>
2025-01-21 11:22:11 -0800

    created footer component and added to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ReactQueryProvider from "@/components/ReactQueryProvider";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import CssBaseline from "@mui/material/CssBaseline";
 import ThemeContextProvider from "./theme/providers";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
               <Navbar />
               <CssBaseline /> {/* Ensures consistent baseline styles */}
               {children} {/* TODO: Add Wrapper Div component after replacing CSS with MUI for consistency*/}
+              <Footer />
             </ReactQueryProvider>
           </AppRouterCacheProvider>
         </ThemeContextProvider>
