f9afe3190c23e18058e181227a81a13b61fc1d16
CourtneyHoppus <cobaltab@gmail.com>
2025-01-23 13:50:01 -0800

    updated per PR review

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,12 @@ import white_nsc_logo from "../public/images/white_vertical_nsc_logo.png";
 
 
 const styles = {
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        marginTop: "100vh",
+        justifyContent: "space-between"
+    },
     mainDiv: {
         display: "flex",
         backgroundColor: 'primary.main',
@@ -21,80 +27,82 @@ const styles = {
 export default function Footer(){
 
     return(
-        <Stack direction="row" sx={styles.mainDiv}>
-            <Stack>
-                <Stack direction="row" color="white" sx={styles.div}>
-                    <Typography variant="h5">
-                        Find out what North Seattle College can offer you.
-                    </Typography>
-                    <Button variant="contained" color="secondary" href="https://northseattle.edu/enroll-now">
-                        Get Started
-                    </Button>
-                    <Button variant="contained" color="secondary" href="https://go.seattlecolleges.edu/form/inquirynorth" target="_blank" rel="noopener">
-                        Request Info
-                    </Button>
-                </Stack>
-                <Stack direction="row" color="white" sx={styles.div}>
-                    <Box>
-                        <Typography variant="h6">
-                        North Seattle College
-                        <br/>
-                        9600 College Way North
-                        <br/>
-                        Seattle, WA 98103
-                        <br/>
-                        206.934.3600
+        <Stack sx={styles.root}>
+            <Stack direction="row" sx={styles.mainDiv}>
+                <Stack>
+                    <Stack direction="row" color="white" sx={styles.div}>
+                        <Typography variant="h5">
+                            Find out what North Seattle College can offer you.
                         </Typography>
-                    </Box>
-                    <Stack paddingLeft={10}>
-                        <Link href="https://northseattle.edu/about/contact-us" underline="hover" color="inherit" variant="h6">
-                            Contact Us
-                        </Link>
-                        <Link href="https://northseattle.edu/about/visit-us" underline="hover" color="inherit" variant="h6">
-                            Visit Us
-                        </Link>
+                        <Button variant="contained" color="secondary" href="https://northseattle.edu/enroll-now">
+                            Get Started
+                        </Button>
+                        <Button variant="contained" color="secondary" href="https://go.seattlecolleges.edu/form/inquirynorth" target="_blank" rel="noopener">
+                            Request Info
+                        </Button>
                     </Stack>
-                    <Stack paddingLeft={10}>
-                        <Link href="https://www.seattlecolleges.edu/" underline="hover" color="inherit">
-                            Seattle Colleges
+                    <Stack direction="row" color="white" sx={styles.div}>
+                        <Box>
+                            <Typography variant="h6">
+                            North Seattle College
+                            <br/>
+                            9600 College Way North
+                            <br/>
+                            Seattle, WA 98103
+                            <br/>
+                            206.934.3600
+                            </Typography>
+                        </Box>
+                        <Stack paddingLeft={10}>
+                            <Link href="https://northseattle.edu/about/contact-us" underline="hover" color="inherit" variant="h6">
+                                Contact Us
+                            </Link>
+                            <Link href="https://northseattle.edu/about/visit-us" underline="hover" color="inherit" variant="h6">
+                                Visit Us
+                            </Link>
+                        </Stack>
+                        <Stack paddingLeft={10}>
+                            <Link href="https://www.seattlecolleges.edu/" underline="hover" color="inherit">
+                                Seattle Colleges
+                            </Link>
+                            <Link href="https://northseattle.edu/" underline="hover" color="inherit">
+                                North Seattle
+                            </Link>
+                            <Link href="https://southseattle.edu/" underline="hover" color="inherit">
+                                South Seattle
+                            </Link>
+                            <Link href="http://www.seattlecentral.edu/" underline="hover" color="inherit">
+                                Central Seattle
+                            </Link>
+                        </Stack>
+                    </Stack>
+                    <Stack direction="row" color="white" sx={styles.div}
+                        divider={<Divider orientation="vertical" flexItem sx={{ backgroundColor: "white" }}/>}>
+                        <Link href="https://northseattle.edu/disability-services"
+                            color="inherit" underline="hover" variant="button">
+                            Accessibility
                         </Link>
-                        <Link href="https://northseattle.edu/" underline="hover" color="inherit">
-                            North Seattle
+                        <Link href="https://northseattle.edu/about/policies-and-procedures/disclaimer"
+                            color="inherit" underline="hover" variant="button">
+                            Disclaimer
                         </Link>
-                        <Link href="https://southseattle.edu/" underline="hover" color="inherit">
-                            South Seattle
+                        <Link href="https://www.seattlecolleges.edu/about/privacy-notice"
+                            color="inherit" underline="hover" variant="button">
+                            Privacy
                         </Link>
-                        <Link href="http://www.seattlecentral.edu/" underline="hover" color="inherit">
-                            Central Seattle
+                        <Link href="https://www.seattlecolleges.edu/administration/human-resources/public-records-requests" 
+                            color="inherit" underline="hover" variant="button">
+                            Public Records
+                        </Link>
+                        <Link href="https://seattlecolleges.formstack.com/forms/north_website_feedback" 
+                            color="inherit" underline="hover" variant="button">
+                            Website Feedback
                         </Link>
                     </Stack>
                 </Stack>
-                <Stack direction="row" color="white" sx={styles.div}
-                    divider={<Divider orientation="vertical" flexItem sx={{ backgroundColor: "white" }}/>}>
-                    <Link href="https://northseattle.edu/disability-services"
-                        color="inherit" underline="hover" variant="button">
-                        Accessibility
-                    </Link>
-                    <Link href="https://northseattle.edu/about/policies-and-procedures/disclaimer"
-                        color="inherit" underline="hover" variant="button">
-                        Disclaimer
-                    </Link>
-                    <Link href="https://www.seattlecolleges.edu/about/privacy-notice"
-                        color="inherit" underline="hover" variant="button">
-                        Privacy
-                    </Link>
-                    <Link href="https://www.seattlecolleges.edu/administration/human-resources/public-records-requests" 
-                        color="inherit" underline="hover" variant="button">
-                        Public Records
-                    </Link>
-                    <Link href="https://seattlecolleges.formstack.com/forms/north_website_feedback" 
-                        color="inherit" underline="hover" variant="button">
-                        Website Feedback
-                    </Link>
+                <Stack justifyContent={"center"} paddingRight={3}>
+                    <Image src={white_nsc_logo} alt="north seattle college logo" height={10} width={200} />
                 </Stack>
-            </Stack>
-            <Stack justifyContent={"center"} paddingRight={3}>
-                <Image src={white_nsc_logo} alt="north seattle college logo" height={10} width={200} />
             </Stack>
         </Stack>
     )
