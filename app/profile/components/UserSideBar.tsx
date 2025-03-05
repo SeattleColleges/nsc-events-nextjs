bafe1cf7b7b070ee1b642547d82e2e932dc2b78c
Bennett Fife <bennett.fife@gmail.com>
2025-03-05 12:32:35 -0800

    create UserContext, integrate context with User Profile components, add buttons to UserSideBar

--- app/profile/components/UserSideBar.tsx
+++ app/profile/components/UserSideBar.tsx
@@ -1,11 +1,28 @@
-import { Avatar, Box, Typography } from '@mui/material'
-import React from 'react'
+import { Avatar, Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import EditUserDetailsDialog, { User } from './EditUserDetailsDialog';
+import router from 'next/router';
+import { set } from 'mongoose';
+import { useUser } from '@/context/UserContext';
 
-interface UserSideBarProps {
-    email: string;
-}
 
-const UserSideBar: React.FC<UserSideBarProps> = ({ email }) => {
+const UserSideBar = () => {
+
+  const { user, setUser } = useUser();
+  
+  const [openEditDialog, setOpenEditDialog] = useState(false);
+
+  const handleEditClick = () => {
+      setOpenEditDialog(true);
+    };
+  
+  const handleCloseEditDialog = (updatedUser?: User) => {
+    setOpenEditDialog(false);
+    if (updatedUser) {
+        setUser(updatedUser);
+    }
+  };
+
   return (
     <Box 
         sx={{
@@ -21,8 +38,21 @@ const UserSideBar: React.FC<UserSideBarProps> = ({ email }) => {
             boxShadow: 3
         }}>
         <Avatar sx={{ height: 140, width: 140, fontSize: '50px', marginBottom: '25px' }}>JD</Avatar>
+        
         <Typography >Email:</Typography>
-        <Typography>{email}</Typography>
+        { user && (
+        <>
+          <Typography>{user.email}</Typography>
+          <Button onClick={handleEditClick} sx={{ mt: 2 }}>Edit Profile</Button>
+          <Button onClick={ () => router.replace('/auth/change-password') }>
+              Change Password
+          </Button>
+        
+        {openEditDialog && (
+            <EditUserDetailsDialog open={openEditDialog} onClose={handleCloseEditDialog} user={user} />
+        )}
+        </>
+        )}
         {/* Date Created and Date Last Edited (needs to be implemented in fetch user) */}
         {/* Edit/Delete Buttons for User */}
     </Box>
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,14 +3,14 @@
 import { getCurrentUserId } from "@/utility/userUtils";
 import { Box, Button, Stack, Typography, useMediaQuery, useTheme } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import CurrentUserCard from "@/app/profile/components/CurrentUserCard";
 import { useRouter } from "next/navigation";
 import useAuth from "@/hooks/useAuth";
 import UnauthorizedPageMessage from "@/components/UnauthorizedPageMessage";
-import EditUserDetailsDialog, { User } from "@/app/profile/components/EditUserDetailsDialog";
+import { User } from "@/app/profile/components/EditUserDetailsDialog";
 import UserHeader from "./components/UserHeader";
 import UserSideBar from "./components/UserSideBar";
 import UserContent from "./components/UserContent";
+import { UserProvider } from "@/context/UserContext";
 
 const URL = process.env.NSC_EVENTS_PUBLIC_API_URL;
 
@@ -18,7 +18,7 @@ const Profile = () => {
     const { isAuth } = useAuth();
     const [user, setUser] = useState<User>();
     const [token, setToken] = useState<string | null>();
-    const [openEditDialog, setOpenEditDialog] = useState(false);
+    
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
     const router = useRouter();
@@ -34,17 +34,7 @@ const Profile = () => {
         const userId = getCurrentUserId();
         getUserFromId(userId);
     },[]);
-
-    const handleEditClick = () => {
-        setOpenEditDialog(true);
-      };
-    
-      const handleCloseEditDialog = (updatedUser?: User) => {
-        setOpenEditDialog(false);
-        if (updatedUser) {
-          setUser(updatedUser);
-        }
-      };    
+        
 
     if (token === null) {
         return (
@@ -63,31 +53,24 @@ const Profile = () => {
         )
     }
   
-    if (isAuth) {
+    if (isAuth && user) {
         return (
-            <>
-                <Typography component="h1" variant="h4" sx={{ textAlign: "center", mt: 3, mb: 3 }}>
-                    Welcome, { user?.firstName }!
-                </Typography>
-                <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: "center", margin:'auto', width: isMobile ? '75%' : '90%' }}>
+            <> 
+                <UserProvider>
+                    <Typography component="h1" variant="h4" sx={{ textAlign: "center", mt: 3, mb: 3 }}>
+                        Welcome, { user?.firstName }!
+                    </Typography>
+                    <Box sx={{ display: "flex", flexDirection: 'column', justifyContent: "center", margin:'auto', width: isMobile ? '75%' : '90%' }}>
+                            
+                        <UserHeader firstName={user.firstName} lastName={user.lastName} pronouns={user.pronouns} /> 
                         
-                    <UserHeader firstName={user.firstName} lastName={user.lastName} pronouns={user.pronouns} /> 
-                    
-                    <Box sx={{ display: "flex", justifyContent: "flex-start", width: "auto", marginY: '10px' }}>
-                        <UserSideBar email={user.email}/> 
-                        <UserContent 
-                        />
+                        <Box sx={{ display: "flex", justifyContent: "flex-start", width: "auto", marginY: '10px' }}>
+                            <UserSideBar/> 
+                            <UserContent 
+                            />
+                        </Box>
                     </Box>
-                    {/* <CurrentUserCard user={user}/>
-                    <Button onClick={handleEditClick} sx={{ mt: 2 }}>Edit Profile</Button>
-                    <Button onClick={ () => router.replace('/auth/change-password') }>
-                        Change Password
-                    </Button>
-                    {openEditDialog && (
-                        <EditUserDetailsDialog open={openEditDialog} onClose={handleCloseEditDialog} user={user} />
-                    )} */}
-                    
-                </Box>
+                </UserProvider>
             </>
         );
     } else {
